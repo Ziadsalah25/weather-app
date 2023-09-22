5289a54a895af5a729c7c0899f55b70dfe085a9f
Ziadsalah25 <zizoms2511@gmail.com>
2023-09-22 15:57:16 +0300

    3d commit

--- src/components/WeatherApp/WeatherApp.jsx
+++ src/components/WeatherApp/WeatherApp.jsx
@@ -24,9 +24,19 @@ const WeatherApp = () => {
       </div>
       <div className="weather-temp">24°c</div>
       <div className="weather-location">London</div>
-      <div className='data-container'>
+      <div className="data-container"></div>
         <div className="element">
-            
+            <img src={humidity_icon} alt="" className="icon"/>
+            <div className="data">
+                <div className="humidity-percent">64%</div>
+                <div className="text">Humidity</div>
+          </div>
+        </div>
+      <div className="element">
+            <img src={wind_icon} alt="" className="icon"/>
+            <div className="data">
+                <div className="humidity-percent">18 km/h</div>
+                <div className="text">Wind Speed</div>
         </div>
       </div>
     </div>

0c2b08d45945120cba6909d0c4dd7fa144e8ef8b
Ziadsalah25 <zizoms2511@gmail.com>
2023-09-24 00:17:53 +0300

    last commits

--- src/components/WeatherApp/WeatherApp.jsx
+++ src/components/WeatherApp/WeatherApp.jsx
@@ -31,36 +31,44 @@ const search = async ()  => {
     const temprature =document.getElementsByClassName("weather-temp");
     const location =document.getElementsByClassName("weather-location");
     
-    humidity[0].innerHTML = data.main.humidity+" %";
-    wind[0].innerHTML = data.wind.speed+" km/h";
-    temprature[0].innerHTML = data.main.temp+"°c";
+    humidity[0].innerHTML = Math.floor(data.main.humidity)+" %";
+    wind[0].innerHTML =Math.floor (data.wind.speed)+" km/h";
+    temprature[0].innerHTML =Math.floor(data.main.temp)+"°c";
     location[0].innerHTML =data.name;
 
-    if(data.weather[0].icon==="01d" ||data.weather[0].icon==="01n" );
+    if(data.weather[0].icon==="01d" ||data.weather[0].icon==="01n" )
     {
-         setWicon(clear_icon);
+        setWicon(clear_icon);
     }
-      else if(data.weather[0].icon==="02d" ||data.weather[0].icon==="02n" );
+      else if(data.weather[0].icon==="02d" ||data.weather[0].icon==="02n" )
       {
           setWicon(cloud_icon);
       }
-      else if(data.weather[0].icon==="03d" ||data.weather[0].icon==="03n" );
+      else if(data.weather[0].icon==="03d" ||data.weather[0].icon==="03n" )
       {
           setWicon(drizzle_icon);
       }
-      else if(data.weather[0].icon==="04d" ||data.weather[0].icon==="04n" );
+      else if(data.weather[0].icon==="04d" ||data.weather[0].icon==="04n" )
       {
           setWicon(drizzle_icon);
       }
-      else if(data.weather[0].icon==="09d" ||data.weather[0].icon==="09n" );
+      else if(data.weather[0].icon==="09d" ||data.weather[0].icon==="09n" )
       {
           setWicon(rain_icon);
       }
-      else if(data.weather[0].icon==="10d" ||data.weather[0].icon==="10n" );
+      else if(data.weather[0].icon==="10d" ||data.weather[0].icon==="10n" )
       {
-          setWicon(cloud_icon);
+          setWicon(rain_icon);
       }
-  }
+      else if(data.weather[0].icon==="13d" ||data.weather[0].icon==="13n" )
+      {
+          setWicon(snow_icon);
+      }
+      else
+      {
+         setWicon(clear_icon);
+      }
+}
 
   return (
     <div className='container'>
